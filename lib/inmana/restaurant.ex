0ad1c89366b0420836c6196b48db3fcde37aaec5
GSushi <gmarcieloliveira@gmail.com>
2021-04-28 20:25:26 -0300

    build: dia 1 e 2 nlw5 elixir

---
+++ lib/inmana/restaurant.ex
@@ -0,0 +1,26 @@
+defmodule Inmana.Restaurant do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @require_params [:email, :name]
+
+  @derive {Jason.Encoder, only: @require_params ++ [:id]}
+
+  schema "restaurants" do
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @require_params)
+    |> validate_required(@require_params)
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+  end
+end
